ed5f33c1b3f526fbf80bc85b0b442bac9e90091a
Mila Grigoryeva <mila.grigoryeva@nextfour.com>
2021-01-20 20:16:43 +0200

    Add ex 5 with the square

--- chapter2/5.cpp
+++ chapter2/5.cpp
@@ -7,6 +7,26 @@ using std::string;
 
 int main()
 {
+    // square
+    const int side = 4;
+    for (int i = 0; i < side; ++i){
+        for (int j = 0; j < side; ++j){
+            if (i == 0 || i == side - 1) {
+                cout << "a ";
+            }
+            else {
+                if (j == 0 || j == side - 1) {
+                    cout << "a ";
+                } 
+                else {
+                    cout << "  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    // rect
+    // triangle 
     
 
     return 0;
